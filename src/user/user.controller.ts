ff2a37e5ef70eb7e86eec3fedfce2107a33f3418
jinhyeok15 <ekfrhd97@gmail.com>
2024-09-27 22:31:34 +0900

    fastify 설정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
+  constructor() {}
+
   @Get('me')
   async findMe() {
     return 'My account';
