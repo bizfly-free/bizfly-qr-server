c492d5e538bbe5bebe032e376a4a9d644ab26b01
jinhyeok15 <ekfrhd97@gmail.com>
2024-09-30 22:19:28 +0900

    import path 일부 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { AppController } from '@bizfly/app.controller';
 import { AppService } from '@bizfly/app.service';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '@bizfly/user/user.module';
 
 import { Module } from '@nestjs/common';
 
